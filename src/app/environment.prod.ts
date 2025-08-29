88d685c852f2559d9b425f83253ca6adf1c47d74
tehnumberone <emre_ftw@live.nl>
2025-08-30 01:12:39 +0200

    lol

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: false,
+    production: true,
     firebaseConfig: {
-        apiKey: "YOUR_FIREBASE_API_KEY",
-        authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-        databaseURL: "YOUR_FIREBASE_DATABASE_URL",
-        projectId: "YOUR_FIREBASE_PROJECT_ID",
-        storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-        appId: "YOUR_FIREBASE_APP_ID"
+        apiKey:  process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        databaseURL: process.env['FIREBASE_DATABASE_URL'],
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['FIREBASE_APP_ID']
     }
 };
