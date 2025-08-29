6380813b77ccdb2f95615880781a856411a7ba1d
tehnumberone <emre_ftw@live.nl>
2025-08-29 23:45:35 +0200

    delweetus

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || "",
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || "",
-        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'] || "",
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || "",
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "",
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "",
-        appId: process.env['NG_APP_FIREBASE_APP_ID'] || ""
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        databaseURL: process.env['FIREBASE_DATABASE_URL'],
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['FIREBASE_APP_ID']
     }
 };
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || "AIzaSyB2e36wu47-6DOc2_rAuHfiFTq9vm6GBD0",
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || "cabbingo-db.firebaseapp.com",
-        databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'] || "https://cabbingo-db-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || "cabbingo-db",
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "cabbingo-db.firebasestorage.app",
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "684304316440",
-        appId: process.env['NG_APP_FIREBASE_APP_ID'] || "1:684304316440:web:0660b0d72a68752cadeb6d"
+        apiKey: "AIzaSyB2e36wu47-6DOc2_rAuHfiFTq9vm6GBD0",
+        authDomain: "cabbingo-db.firebaseapp.com",
+        databaseURL: "https://cabbingo-db-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "cabbingo-db",
+        storageBucket: "cabbingo-db.firebasestorage.app",
+        messagingSenderId: "684304316440",
+        appId: "1:684304316440:web:0660b0d72a68752cadeb6d"
     }
 };
