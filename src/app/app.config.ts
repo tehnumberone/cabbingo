b8d24cf681e077996e6ddcf980baf5ca1dfbda68
tehnumberone <emre_ftw@live.nl>
2025-08-30 15:22:32 +0200

    typeshit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay, withNoHttpTransferCache } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideDatabase, getDatabase } from '@angular/fire/database'; // Changed from Firestore
-
 import { environment } from './environment';
 
 export const appConfig: ApplicationConfig = {
