37186c246991edaafc49146925b81a6d3a8ad4c3
tehnumberone <emre_ftw@live.nl>
2025-09-02 23:45:26 +0200

    double name change type shit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cabbingov2
+# Cabbingo
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.6.
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cabbingov2');
+  protected readonly title = signal('cabbingo');
 }
